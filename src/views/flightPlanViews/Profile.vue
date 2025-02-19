3a7ed23f96a4a72b28c9a79d259f73b1a237636a
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-02-19 16:48:40 -0600

    Update (profile, settings) Added break lines
    Added break lines in the profile and settings pages so that the dropdown menus can be seen

--- src/views/flightPlanViews/Profile.vue
+++ src/views/flightPlanViews/Profile.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         This is the profile page
+        <br><br><br><br><br><br><br><br><br><br><br><br><br> <!-- Temporary to show dropdown menu -->
     </div>
 </template>
 
