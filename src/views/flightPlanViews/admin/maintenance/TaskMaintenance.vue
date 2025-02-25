889d6b6a47073683aadb78b1d186a8ed27691aff
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-02-25 04:50:48 -0600

    Feature (data table) Table displaying tasks
    Table displaying tasks uploaded from seeder with name, description, point value, category, and the reflection (if any) required

--- src/views/flightPlanViews/admin/maintenance/TaskMaintenance.vue
+++ src/views/flightPlanViews/admin/maintenance/TaskMaintenance.vue
@@ -1,13 +1,63 @@
 <template>
-    <div>
-        This is the Task Maintenance Page
-    </div>
-</template>
+    <v-card
+      title="Tasks"
+      flat
+    >
+      <template v-slot:text>
+        <v-text-field
+          v-model="search"
+          label="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="outlined"
+          hide-details
+          single-line
+        ></v-text-field>
+      </template>
+  
+      <v-data-table
+        :headers="headers"
+        :items="tasks"
+        :search="search"
+        :v-model="selected"
+      ></v-data-table>
+    </v-card>
+  </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import TaskServices from '@/services/flightPlanServices/taskServices';
 
+// Reactive references for state
+const search = ref('');
+const tasks = ref([]);
+const message = ref('');
+const headers = ref([
+  { align: 'start', key: 'name', title: 'Name' },
+  { key: 'description', title: 'Description' },
+  { key: 'category', title: 'Category' },
+  { key: 'point_value', title: 'Point Value' },
+  { key: 'reflection', title: 'Reflection Required?' },
+]);
+
+onMounted(() => {
+  TaskServices.getAllTasks()
+    .then((res) => {
+      tasks.value = res.data; 
+      message.value = '';
+      console.log(tasks.value);
+    })
+    .catch((err) => {
+      message.value = `Error: ${err.code}: ${err.message}`; // Show error message
+      console.error(err);
+    });
+});
 </script>
 
-<style scope>
+<style>
+    /* @import stylesheet */
+</style>
+
+
+<style scoped>
 
 </style>
