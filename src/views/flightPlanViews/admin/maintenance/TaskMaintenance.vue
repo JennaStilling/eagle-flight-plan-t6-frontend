bd5f280bb980fdf1d3fd1bdf8fd8d278b1274d7b
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-02-26 12:28:48 -0600

    Feature (add/update modal) Added base
    Added base modal for add/updating task information - not formatted or functional yet

--- src/views/flightPlanViews/admin/maintenance/TaskMaintenance.vue
+++ src/views/flightPlanViews/admin/maintenance/TaskMaintenance.vue
@@ -3,15 +3,15 @@
         <div class="title-row">
             <h1 class="table-title">Tasks</h1>
             <div class="search-filter-button-group">
-                <v-text-field v-model="search" label="Search" variant="solo" hide-details single-line
+                <v-text-field v-model="search" label="Search" variant="solo-filled" hide-details single-line
                     density="compact" class="search-bar">
                     <template v-slot:prepend-inner>
                         <Icon icon="material-symbols:search-rounded" width="24" height="24" />
                     </template>
                 </v-text-field>
 
-                <v-select v-model="selectedFilter" :items="filterOptions" label="Filter By Category" variant="solo"
-                    density="compact" hide-details class="filter-menu"></v-select>
+                <v-select v-model="selectedFilter" :items="filterOptions" label="Filter By Category"
+                    variant="solo-filled" density="compact" hide-details class="filter-menu"></v-select>
 
                 <v-btn class="button" variant="elevated" color="#5EC4B6" @click="addTask()">
                     Add Task
@@ -22,9 +22,10 @@
             </div>
         </div>
 
-        <v-data-table :headers="headers" :items="filteredTasks" :search="search" v-model:selectable="selected" show-select>
+        <v-data-table :headers="headers" :items="filteredTasks" :search="search" v-model:selectable="selected"
+            show-select>
             <template v-slot:[`item.actions`]="{ item }">
-                <v-btn variant="plain" size="small" @click="editTask(item)">
+                <v-btn variant="plain" size="small" @click="editTaskPopup(item)">
                     <Icon icon="material-symbols:edit-outline" width="24" height="24" />
                 </v-btn>
                 <v-btn variant="plain" size="small" @click="deleteTaskConfirmation(item)">
@@ -35,27 +36,81 @@
     </v-card>
 
     <div v-if="showDeleteItem" class="modal">
-    <div class="modal-content">
-      <div class="modal-header">
-        <span @click="showDeleteItem = false" class="close">&times;</span>
-        <p v-if="!deleteError">
-          Do you want to DELETE <br />
-          {{ taskToDelete.name}}?
-        </p>
-        <p v-if="deleteError">
-          Error deleting<br />{{ taskToDelete.name }}.
-        </p>
-      </div>
-      <div class="modal-body">
-        <v-btn v-if="!deleteError" color="#708E9A" @click="showDeleteItem = false">CANCEL</v-btn>
-        <v-btn v-if="!deleteError" color="#F04E3E" class="error" @click="deleteTask(task)">DELETE</v-btn>
-        <v-btn v-if="deleteError" @click="
-          deleteError = false;
-        showDeleteItem = false;
-        ">CLOSE</v-btn>
-      </div>
+        <div class="modal-content">
+            <div class="modal-header">
+                <span @click="showDeleteItem = false" class="close">&times;</span>
+                <p v-if="!deleteError">
+                    Do you want to DELETE <br />
+                    {{ taskToDelete.name }}?
+                </p>
+                <p v-if="deleteError">
+                    Error deleting<br />{{ taskToDelete.name }}.
+                </p>
+            </div>
+            <div class="modal-body">
+                <v-btn v-if="!deleteError" color="#708E9A" @click="showDeleteItem = false">CANCEL</v-btn>
+                <v-btn v-if="!deleteError" color="#F04E3E" class="error" @click="deleteTask(task)">DELETE</v-btn>
+                <v-btn v-if="deleteError" @click="
+                    deleteError = false;
+                showDeleteItem = false;
+                ">CLOSE</v-btn>
+            </div>
+        </div>
     </div>
-  </div>
+
+    <div v-if="showTaskDetails" class="modal">
+        <div class="modal-content">
+            <!-- <div class="modal-header">
+                <span @click="showTaskDetails = false" class="close">&times;</span>
+                 <p v-if="editError">
+                    Error editing <br />{{ taskToEdit.name }}.
+                </p>
+            </div> -->
+            <div class="modal-body">
+                <v-card class="mx-auto" max-width="344">
+                    <v-container>
+                        <v-text-field v-if="taskEdit" v-model="taskName" color="primary"
+                            variant="underlined">{{ taskToEdit.name }}</v-text-field>
+
+                            <v-text-field v-else v-model="taskName" color="primary" placeholder="<Task Name>"
+                                variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="taskCategory" color="primary" placeholder="<Task Category>"
+                            variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="isRequired" color="primary" placeholder="<Is Required?>" variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="scheduleType" color="primary" placeholder="<Schedule Type>" variant="underlined"></v-text-field>
+
+                        <v-textarea v-model="taskDescription"       rows="3"
+                        no-resize color="primary" placeholder="<Task Description>" variant="underlined"></v-textarea>
+
+                        <v-text-field v-model="taskRationale" color="primary" placeholder="<Rationale>" variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="semFromGrad" color="primary" placeholder="<Number of Semesters>" variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="taskPointValue" color="primary" placeholder="<Point Value>" variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="taskPreReq" color="primary" placeholder="<Search Task>" variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="taskVideoLink" color="primary" placeholder="<Video Link>" variant="underlined"></v-text-field>
+
+                        <v-text-field v-model="taskVerificationType" color="primary" placeholder="<Search Verifications>" variant="underlined"></v-text-field>
+                    </v-container>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+
+                        <v-btn color="success">
+                            Complete Registration
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -67,10 +122,16 @@ const search = ref('');
 const tasks = ref([]);
 const message = ref('');
 const selected = ref([]);
+const showTaskDetails = ref(false);
 const showDeleteItem = ref(false);
 const deleteError = ref(false);
+const editError = ref(false);
 const selectedFilter = ref('All');
+
+const taskToEdit = ref(null);
 const taskToDelete = ref(null);
+const taskEdit = ref(false);
+const taskAdd = ref(false);
 
 const headers = ref([
     { align: 'start', key: 'name', title: 'Name' },
@@ -110,8 +171,32 @@ onMounted(() => {
         });
 });
 
-const editTask = (task) => {
-    console.log('Edit task:', task.name);
+export {
+    data: () => ({
+      form: false,
+      email: null,
+      password: null,
+      loading: false,
+    }),
+}
+
+const editTaskPopup = (task) => {
+    taskToEdit.value = task;
+    showTaskDetails.value = true;
+    taskEdit.value = true;
+    taskAdd.value = false;
+};
+
+const editTask = () => {
+    console.log('Edit task:', taskToEdit.value.name);
+    showTaskDetails.value = false
+}
+
+const addTask = () => {
+    console.log("Adding a task...");
+    showTaskDetails.value = true;
+    taskAdd.value = true;
+    taskEdit.value = false;
 };
 
 const deleteTaskConfirmation = (task) => {
@@ -122,14 +207,14 @@ const deleteTaskConfirmation = (task) => {
 
 const deleteTask = () => {
     TaskServices.deleteTask(taskToDelete.value.id)
-    .then(() => {
-      showDeleteItem.value = false;
-      tasks.value = tasks.value.filter((allTasks) => allTasks.id !== taskToDelete.value.id);
-    })
-    .catch((e) => {
-      message.value = e.response.data.message;
-      deleteError.value = true;
-    });
+        .then(() => {
+            showDeleteItem.value = false;
+            tasks.value = tasks.value.filter((allTasks) => allTasks.id !== taskToDelete.value.id);
+        })
+        .catch((e) => {
+            message.value = e.response.data.message;
+            deleteError.value = true;
+        });
 };
 
 const deleteSelectedTasks = (selected) => {
@@ -137,18 +222,21 @@ const deleteSelectedTasks = (selected) => {
         console.log("Deleting selected tasks: ", selected);
         selected.forEach(task => {
             console.log("Deleting task: ", task.name);
-            // Call delete API or logic for individual task deletion
+            TaskServices.deleteTask(task.id)
+                .then(() => {
+                    showDeleteItem.value = false;
+                    tasks.value = tasks.value.filter((allTasks) => allTasks.id !== task.id);
+                })
+                .catch((e) => {
+                    message.value = e.response.data.message;
+                    deleteError.value = true;
+                });
         });
     } else {
         console.log("No tasks selected.");
     }
 }
 
-
-
-const addTask = () => {
-    console.log("Adding a task...");
-};
 </script>
 
 
@@ -198,5 +286,9 @@ const addTask = () => {
     width: auto;
     color: white !important;
     white-space: nowrap;
+}
+
+.button-white-text {
+    color: white !important;
 }
 </style>
