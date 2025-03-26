25c909f239a924d3cab10dbb26203223c0e37041
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-03-26 14:30:59 -0500

    Update (event list) Students can click on event for details
    Student can click on the event to view all details. Each field is disabled

--- src/views/flightPlanViews/student/StudentEvents.vue
+++ src/views/flightPlanViews/student/StudentEvents.vue
@@ -85,8 +85,155 @@
         </div>
 
         <div v-if="!showCalendarView">
-            <v-data-table :headers="headers" :items="filteredEvents" :search="search">
+            <v-data-table :headers="headers" :items="filteredEvents" :search="search"
+                @click:row="(event, { item }) => editEventPopup(item)">
             </v-data-table>
+        </div>
+
+        <div v-if="showEventDetails" class="modal edit-form-body">
+            <v-card class="edit-popup mx-auto">
+                <v-card-title class="popup-header">
+                    <v-text-field v-model="eventName" variant="outlined" density="compact" hide-details disabled>
+                    </v-text-field>
+                </v-card-title>
+
+                <v-container>
+                    <!-- Description-->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>{{ labels.description }}</label>
+                        </v-col>
+                        <v-col cols="7">
+                            <v-textarea v-model="eventDescription" rows="8" variant="outlined" density="compact"
+                                disabled></v-textarea>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Type-->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>{{ labels.type }}</label>
+                        </v-col>
+
+                        <v-col cols="7">
+                            <v-select v-model="eventType" :items="eventTypes" variant="solo-filled" density="compact"
+                                hide-details class="filter-menu" disabled></v-select>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Start Date -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>Start Date</label>
+                        </v-col>
+                        <v-col cols="7">
+                            <v-text-field v-model="eventStartDate" type="date" variant="outlined" density="compact"
+                                hide-details @update:model-value="updateEndDate" disabled></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <!-- End Date -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>End Date</label>
+                        </v-col>
+                        <v-col cols="7">
+                            <v-text-field v-model="eventEndDate" type="date" variant="outlined" density="compact"
+                                hide-details :min="eventStartDate" disabled></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Start Time -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>Start Time</label>
+                        </v-col>
+
+                        <v-col cols="7">
+                            <v-text-field v-model="eventStartTime" type="time" variant="outlined" density="compact"
+                                hide-details disabled></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <!-- End Time  -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>End Time</label>
+                        </v-col>
+
+                        <v-col cols="7">
+                            <v-text-field v-model="eventEndTime" type="time" variant="outlined" density="compact"
+                                hide-details disabled></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Location  -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>{{ labels.location }}</label>
+                        </v-col>
+
+                        <v-col cols="7">
+                            <v-text-field v-model="eventLocation" variant="outlined" density="compact" hide-details
+                                disabled></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Attendance Type -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>{{ labels.attendance }}</label>
+                        </v-col>
+
+                        <v-col cols="7">
+                            <v-select v-model="eventAttendanceType" :items="attendanceTypes" variant="solo-filled"
+                                density="compact" hide-details class="filter-menu" disabled></v-select>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Custom Event -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>{{ labels.custom }}</label>
+                        </v-col>
+                        <v-col cols="7">
+                            <v-checkbox v-model="eventCustomEvent" hide-details disabled></v-checkbox>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Status - -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>{{ labels.status }}</label>
+                        </v-col>
+
+                        <v-col cols="7">
+                            <v-select v-model="eventStatus" :items="statusOptions" variant="solo-filled"
+                                density="compact" hide-details class="filter-menu" disabled></v-select>
+                        </v-col>
+                    </v-row>
+
+                    <!-- Point Value -->
+                    <v-row class="form-row">
+                        <v-col cols="5" class="label-column">
+                            <label>{{ labels.points }}</label>
+                        </v-col>
+
+                        <v-col cols="7">
+                            <v-text-field v-model="eventPointValue" variant="outlined" density="compact" hide-details
+                                disabled></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+
+                <v-divider></v-divider>
+
+                <v-card-actions class="popup-actions">
+                    <v-spacer></v-spacer>
+                    <v-btn color="#708E9A" variant="flat" @click="showEventDetails = false">Close</v-btn>
+
+                </v-card-actions>
+            </v-card>
         </div>
     </div>
 </template>
@@ -164,6 +311,7 @@ const headers = ref([
     { key: 'formatted_time', title: 'Time' },
     { key: 'location', title: 'Location' },
     { key: 'event_type', title: 'Tags', sortable: false },
+    { key: 'point_value', title: 'Points'}
 ]);
 
 const filterOptions = ref(['All']);
@@ -208,6 +356,7 @@ const filteredEvents = computed(() => {
     if (selectedFilter.value === 'All') {
         return events.value.map(event => ({
             ...event,
+            id: event.id,
             formatted_date: formatDate(event.start_date_time || event.date),
             formatted_time: formatTime(event.start_date_time)
         }));
@@ -221,6 +370,7 @@ const filteredEvents = computed(() => {
         return event.type === selectedFilter.value.toLowerCase();
     }).map(event => ({
         ...event,
+        id: event.id,
         formatted_date: formatDate(event.start_date_time || event.date),
         formatted_time: formatTime(event.start_date_time)
     }));
@@ -581,6 +731,7 @@ const addEvent = () => {
     if (eventAttendanceType.value === 'In Person') {
         eventAttendanceType.value = 'in_person'
     }
+
 
     const startDate = parseISO(eventStartDate.value);
     const endDate = parseISO(eventEndDate.value);
