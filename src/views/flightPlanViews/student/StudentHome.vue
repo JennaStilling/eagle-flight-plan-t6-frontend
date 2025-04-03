81c0d2cad9fc0f3ee48c540eeb45bf3848ef1796
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-04-02 19:40:47 -0500

    Update (student services) Added API calls
    Added API calls for getting recommended events

--- src/views/flightPlanViews/student/StudentHome.vue
+++ src/views/flightPlanViews/student/StudentHome.vue
@@ -339,6 +339,7 @@ const goToShop = () => {
   router.push({ name: 'shop' });
 };
 const viewMoreEvents = () => {
+  localStorage.setItem('viewPersonalCalendar', true);
   router.push({ name: 'student-events' });
 };
 
