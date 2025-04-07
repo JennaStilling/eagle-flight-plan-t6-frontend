cd835c36bef54008ef5a4113aef40c136adec49e
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-04-07 00:25:49 -0500

    Fix (merge)

--- src/components/flightPlanComponents/SideBar.vue
+++ src/components/flightPlanComponents/SideBar.vue
@@ -14,45 +14,47 @@
               <Icon :icon="approvalRequestsDropdown ? 'material-symbols:expand-less' : 'material-symbols:expand-more'"
                 class="dropdown-arrow" />
               <div v-if="approvalRequestsDropdown" class="dropdown-menu" @click.stop>
-                <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveEvent' }"
-                    @click="closeSidebar">View Event Approvals</router-link></li>
-                <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveTask' }"
-                    @click="closeSidebar">View Task Approvals</router-link></li>
-                <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveExperience' }"
-                    @click="closeSidebar">View Experience Approvals</router-link></li>
-                                    <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveCustomEvent' }"
-                    @click="closeSidebar">View Custom Event Approvals</router-link></li>
-                <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveCustomTask' }"
-                    @click="closeSidebar">View Custom Task Approvals</router-link></li>
-                <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveCustomExperience' }"
-                    @click="closeSidebar">View Custom Experience Approvals</router-link></li>
-        </div>
+            <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveEvent' }" @click="closeSidebar">View
+                Event Approvals</router-link></li>
+            <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveTask' }" @click="closeSidebar">View
+                Task Approvals</router-link></li>
+            <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveExperience' }"
+                @click="closeSidebar">View Experience Approvals</router-link></li>
+            <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveCustomEvent' }"
+                @click="closeSidebar">View Custom Event Approvals</router-link></li>
+            <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveCustomTask' }"
+                @click="closeSidebar">View Custom Task Approvals</router-link></li>
+            <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'approveCustomExperience' }"
+                @click="closeSidebar">View Custom Experience Approvals</router-link></li>
+      </div>
       </li>
 
       <li @click="toggleMaintenanceDropdown">Maintenance
         <Icon :icon="maintenanceDropdown ? 'material-symbols:expand-less' : 'material-symbols:expand-more'"
           class="dropdown-arrow" />
         <div v-if="maintenanceDropdown" class="dropdown-menu" @click.stop>
-      <li v-if="hasUserPermission"><router-link :to="{ name: 'userMaintenance' }"
-          @click="closeSidebar">Users</router-link></li>
-      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'event' }" @click="closeSidebar">Events</router-link>
+      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'badge' }" @click="closeSidebar">Badges</router-link>
       </li>
-      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'task' }" @click="closeSidebar">Tasks</router-link>
+      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'cliftonStrength' }" @click="closeSidebar">Clifton
+          Strengths</router-link></li>
+      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'event' }" @click="closeSidebar">Events</router-link>
       </li>
       <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'experience' }"
           @click="closeSidebar">Experiences</router-link></li>
       <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'flightPlan' }" @click="closeSidebar">Flight
           Plans</router-link></li>
-      <li v-if="hasShopPermission"><router-link :to="{ name: 'award' }" @click="closeSidebar">Shop
-          Items</router-link></li>
-      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'badge' }" @click="closeSidebar">Badges</router-link>
-      </li>
-      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'cliftonStrength' }" @click="closeSidebar">Clifton
-          Strengths</router-link></li>
       <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'lifeAfterTheNest' }" @click="closeSidebar">Life
           After the Nest</router-link></li>
-      <li v-if="hasShopPermission"><router-link :to="{ name: 'transactionLog' }" @click="closeSidebar">Transaction
-          History</router-link></li>
+      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'major' }" @click="closeSidebar">Majors
+        </router-link></li>
+      <li v-if="hasShopPermission"><router-link :to="{ name: 'award' }" @click="closeSidebar">Shop
+          Items</router-link></li>
+      <li v-if="hasFlightPlanPermission"><router-link :to="{ name: 'task' }" @click="closeSidebar">Tasks</router-link>
+      </li>
+
+      <li v-if="hasUserPermission"><router-link :to="{ name: 'userMaintenance' }"
+          @click="closeSidebar">Users</router-link>
+      </li>
     </div>
     </li>
     <!-- Other Admin Pages -->
