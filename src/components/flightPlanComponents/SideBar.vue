e163e42a92c21c3e6012b819d82755c3659bf079
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-04-14 14:01:58 -0500

    Fix (side bar) Added route to transaction log

--- src/components/flightPlanComponents/SideBar.vue
+++ src/components/flightPlanComponents/SideBar.vue
@@ -60,6 +60,7 @@
     <!-- Other Admin Pages -->
     <template v-if="isAdminViewActive">
       <li><router-link :to="{ name: 'adminHomeFP' }" @click="closeSidebar">Home</router-link></li>
+      <li><router-link :to="{ name: 'transactionLog' }" @click="closeSidebar">Redeem Student Points</router-link></li>
       <li><router-link :to="{ name: 'homeRB' }" @click="closeSidebar">Resume Builder</router-link></li>
     </template>
 </template>
