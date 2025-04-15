1ad462707def685b50b0ac91c83b80ec15ee0a92
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-04-15 16:11:40 -0500

    Update (user profile) Split first and last name fields

--- src/views/flightPlanViews/Profile.vue
+++ src/views/flightPlanViews/Profile.vue
@@ -88,7 +88,8 @@
           </div>
         </div>
 
-        <input type="text" class="name-input" v-model="userName" />
+        <input type="text" class="name-input" v-model="userFirstName" />
+        <input type="text" class="name-input" v-model="userLastName" />
       </div>
 
       <div class="popup-content">
@@ -223,7 +224,8 @@ const name = ref("");
 
 // Update Profile Variables
 const showProfileUpdate = ref(false);
-const userName = ref("");
+const userFirstName = ref("");
+const userLastName = ref("");
 const userPhoneNumber = ref("");
 const userPrefix = ref("");
 const userImage = ref(null);
@@ -266,7 +268,8 @@ const getUser = () => {
       if (userInfo.value.image === null) hasImage.value = false;
       else hasImage.value = true;
 
-      userName.value = userInfo.value.fName + " " + userInfo.value.lName;
+      userFirstName.value = userInfo.value.fName;
+      userLastName.value = userInfo.value.lName;
       userPhoneNumber.value = userInfo.value.phone_number;
       userPrefix.value = userInfo.value.prefix;
       userImage.value = userInfo.value.image;
@@ -515,8 +518,8 @@ const updateUserInfo = () => {
   toggleUpdateModal();
 
   const updateUser = {
-    fName: userName.value.split(' ')[0],
-    lName: userName.value.split(' ')[1],
+    fName: userFirstName.value,
+    lName: userLastName.value,
     email: userInfo.value.email, // Does not change
     phone_number: userPhoneNumber.value,
     prefix: userPrefix.value,
@@ -693,7 +696,7 @@ const fileUpload = (event) => {
   padding-left: 10px;
   height: 150px;
   text-align: left;
-  width: 70%;
+  width: 25%;
   min-width: 400px;
   border-radius: 10px;
   background: rgba(32, 32, 32, 0.15);
