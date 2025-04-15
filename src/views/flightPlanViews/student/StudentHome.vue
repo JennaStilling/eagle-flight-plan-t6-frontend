1c5cbf68ffbbbcb7e249c3c14f205f3f338468aa
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-04-14 21:00:04 -0500

    Update (student home) Only show in app registered events
    Events under "confirm registration" will now only display events that have passed that students sign in through the app itself (registration != handshake)

--- src/views/flightPlanViews/student/StudentHome.vue
+++ src/views/flightPlanViews/student/StudentHome.vue
@@ -321,7 +321,7 @@ onMounted(async () => {
   try {
     const eventResponse = await StudentEventServices.getAllEventsByStudent(user.value.studentId);
     if (eventResponse.data) {
-      events.value = eventResponse.data.filter(event => new Date(event.date) <= new Date(currentDate.value) && event.studentEvent[0].attendance_status === 'registered');
+      events.value = eventResponse.data.filter(event => new Date(event.date) <= new Date(currentDate.value) && event.studentEvent[0].attendance_status === 'registered' && event.studentEvent[0].registration !== 'handshake');
       events.value.sort((a, b) => new Date(a.date) - new Date(b.date));
       pastEvents.value = events.value;
     }
